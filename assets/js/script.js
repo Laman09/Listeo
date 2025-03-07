101490ffd7322d58d12100cbfbcb7991782d2865
Laman09 <ljalilli09@gmail.com>
2025-03-08 01:50:24 +0400

    fourth commit

---
+++ assets/js/script.js
@@ -0,0 +1,27 @@
+document.getElementById('openLink').addEventListener('click', function(e) {
+    e.preventDefault();
+    const href = this.getAttribute('href');
+    window.open(href, '_blank');
+});
+
+document.getElementById('buttonLink').addEventListener('click', function(e) {
+    e.preventDefault();
+    const href = this.getAttribute('href');
+    window.open(href, '_blank');
+});
+
+window.onload = () => {
+    const headings = document.querySelectorAll('h1');
+
+    setTimeout(() => {
+      headings[0].classList.add('show');
+    }, 1000);
+  
+    setTimeout(() => {
+      headings[1].classList.add('show');
+    }, 2500);
+  
+    setTimeout(() => {
+      headings[2].classList.add('show');
+    }, 4000);
+};
