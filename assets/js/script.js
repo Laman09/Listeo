ae7953bda84f2c85c82bbdd51def80e19fddee8f
Laman09 <ljalilli09@gmail.com>
2025-03-08 23:58:00 +0400

    fifth commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,18 +10,24 @@ document.getElementById('buttonLink').addEventListener('click', function(e) {
     window.open(href, '_blank');
 });
 
-window.onload = () => {
-    const headings = document.querySelectorAll('h1');
+document.getElementById('rateLink').addEventListener('click', function(e) {
+    e.preventDefault();
+    const href = this.getAttribute('href');
+    window.open(href, '_blank');
+});
 
-    setTimeout(() => {
-      headings[0].classList.add('show');
-    }, 1000);
+// window.onload = () => {
+//     const headings = document.querySelectorAll('h1');
+
+//     setTimeout(() => {
+//       headings[0].classList.add('show');
+//     }, 1000);
   
-    setTimeout(() => {
-      headings[1].classList.add('show');
-    }, 2500);
+//     setTimeout(() => {
+//       headings[1].classList.add('show');
+//     }, 2500);
   
-    setTimeout(() => {
-      headings[2].classList.add('show');
-    }, 4000);
-};
+//     setTimeout(() => {
+//       headings[2].classList.add('show');
+//     }, 4000);
+// };
